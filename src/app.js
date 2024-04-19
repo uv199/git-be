5bde1c87fd8d20ea1ea59d2e294562a960a0a405
Urvish Patel <uspatel199@gmail.com>
2024-04-19 09:23:43 +0530

    ad

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import { config } from "./config.js";
 import { dbConnect } from "./db/db.js";
 
 const port = config.PORT;
-// process.on("uncaughtException", (error) => {
-//   errorHandler.handleError(error);
-//   if (!errorHandler.isTrustedError(error)) {
-//     process.exit(1);
-//   }
-// });
 
 dbConnect()
   .then(() => {
